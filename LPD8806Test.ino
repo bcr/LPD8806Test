ea8e988bf99d87ef91b3fe8b14a952558b120392
Blake Ramsdell <blaker@gmail.com>
2012-11-28 23:20:57 -0800

    Use SPI not bitbanging, dumbass.

--- LPD8806Test.ino
+++ LPD8806Test.ino
@@ -4,14 +4,10 @@
 // Number of RGB LEDs in strand:
 int nLEDs = 240;
 
-// Chose 2 pins for output; can be any valid output pins:
-int dataPin  = 2;
-int clockPin = 3;
-
 // First parameter is the number of LEDs in the strand.  The LED strips
 // are 32 LEDs per meter but you can extend or cut the strip.  Next two
 // parameters are SPI data and clock pins:
-LPD8806 strip = LPD8806(nLEDs, dataPin, clockPin);
+LPD8806 strip = LPD8806(nLEDs);
 
 void setup()
 {
